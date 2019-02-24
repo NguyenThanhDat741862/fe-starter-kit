8b599e8f42b818da5610440ff08db7ec6503c6db
nguyenThanhDat <nguyenThanhDat741862@gmail.com>
2019-02-24 10:49:46 +0700

    Modify build task

--- gulp/build.js
+++ gulp/build.js
@@ -6,7 +6,8 @@ const img = require('./img_dist');
 const js = require('./js_dist');
 const html = require('./html_dist');
 const clean = require('./clean_dist');
+const tmp = require('./tmp');
 
-let build = series(clean, series( parallel(css, font, img, js) , html) );
+let build = series(clean, tmp, series( parallel(css, font, img, js) , html) );
 
 module.exports = build;
